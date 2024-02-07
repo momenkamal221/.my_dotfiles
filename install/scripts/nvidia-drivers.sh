5dc7c47d85108a06a96737a9e4312d084d698135
momenkamal221 <momenkamal221@gmail.com>
2024-02-07 11:44:26 +0200

    -y add

--- install/scripts/nvidia-drivers.sh
+++ install/scripts/nvidia-drivers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo dnf update -y # and reboot if you are not on the latest kernel
-sudo dnf install akmod-nvidia # rhel/centos users can use kmod-nvidia instead
-sudo dnf install xorg-x11-drv-nvidia-cuda #optional for cuda/nvdec/nvenc support
+sudo dnf install akmod-nvidia -y # rhel/centos users can use kmod-nvidia instead
+sudo dnf install xorg-x11-drv-nvidia-cuda -y #optional for cuda/nvdec/nvenc support
